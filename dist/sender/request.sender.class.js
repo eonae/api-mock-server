e9bec0b9058eac97d0f4e5662dab0390ecfdabf9
Sergey Aslanov <s.aslanov@medpoint24.ru>
2020-01-20 10:20:54 +0300

    Fixes .gitignore

--- dist/sender/request.sender.class.js
+++ dist/sender/request.sender.class.js
@@ -1,17 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var supertest_1 = require("supertest");
-var constants_1 = require("../constants");
-exports.bearer = function (token) { return 'Bearer ' + token; };
-exports.getCsrf = function (res) { return res.header['x-csrf-token']; };
-var RequestSender = (function () {
-    function RequestSender(app) {
+const supertest_1 = __importDefault(require("supertest"));
+const constants_1 = require("../constants");
+exports.bearer = (token) => 'Bearer ' + token;
+exports.getCsrf = (res) => res.header['x-csrf-token'];
+class RequestSender {
+    constructor(app) {
         this.app = app;
     }
-    RequestSender.prototype.sendRequest = function (method, url, options) {
-        var req = supertest_1.default(this.app)[method.toLowerCase()](url);
+    sendRequest(method, url, options) {
+        let req = supertest_1.default(this.app)[method.toLowerCase()](url);
         if (options) {
-            var data = options.data, jwt = options.jwt, csrf = options.csrf, headers = options.headers;
+            const { data, jwt, csrf, headers } = options;
             if (jwt) {
                 req = req.set(constants_1.AUTHORIZATION, exports.bearer(jwt));
             }
@@ -19,8 +22,7 @@ var RequestSender = (function () {
                 req = req.set(constants_1.X_CSRF_TOKEN, csrf);
             }
             if (headers) {
-                Object.entries(options.headers).forEach(function (_a) {
-                    var header = _a[0], value = _a[1];
+                Object.entries(options.headers).forEach(([header, value]) => {
                     req = req.set(header, value);
                 });
             }
@@ -29,8 +31,7 @@ var RequestSender = (function () {
             }
         }
         return req;
-    };
-    return RequestSender;
-}());
+    }
+}
 exports.RequestSender = RequestSender;
 //# sourceMappingURL=request.sender.class.js.map
