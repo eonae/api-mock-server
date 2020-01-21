f0f229bac160f8d913b121c785f74317fb72058f
eonae <eonae.white@gmail.com>
2020-01-22 02:00:59 +0300

    ...

--- bin/index.js
+++ bin/index.js
@@ -4,5 +4,5 @@ const defaultConfig = process.argv.length === 3 && process.argv[2] === '--defaul
 const customConfig = process.argv.length === 4 && process.argv[2] === '--config';
 
 if (defaultConfig || customConfig) {
-  require('./dist/main.js')(customConfig ? process.argv[3] : undefined);
+  require('../dist/main').main(customConfig ? process.argv[3] : undefined);
 }
